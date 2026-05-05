71f76c15e1ba0f9e8119242bf67572e08d621cea
Sujit Shrestha <sujitxtha4222@gmail.com>
2026-05-05 14:58:35 +0545

    making new file for feature engineering updateand updating names too

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,47 +1,50 @@
 # ==============================
 # Retail Sales Prediction App
 # ==============================
-
 import streamlit as st
 import pandas as pd
 import joblib
 
-from analysis import category_lst
+
+# These variables are coming from data_store.py file which we made basically to get the requried datas for our app
+from data_store import category_lst, region_lst, segment_lst, shipmode_lst
+from data_store import category_dict
 
 
 
 # Load trained model
-model = joblib.load("model.pkl")
+model = joblib.load("../data/model_RandomForestRegressor.pkl")
+
+
 
 
 
 
 # App title
 st.title("📊 Retail Sales Prediction App")
-
 st.write("Enter order details to predict estimated sales.")
 
 
 
 
-# User inputs for Category and Sub-category Selector
-category_data = {
-    "Technology": category_lst,
-    "Furniture": ["Bookcase", "Chair", "Table"],
-    "Clothing": ["Shirt", "Pant", "Shoes"]
-}
+
+
+# User inputs for category and sub_category Selector using our dictionary (i.e category_dict)
+# where,
+# key contains categories
+# value contains sub_categories
 
 category = st.selectbox(
     "Select Category",
-    list(category_data.keys())
+    list(category_dict.keys())  #converting key values of category_dict in list(so, it's list of category)
 )
 
 sub_category = st.selectbox(
     "Select Sub-category",
-    category_data[category]
+    category_dict[category]     # means automatically updating the sub-categories based on chosed category
 )
 
-st.write("You selected:")
+st.write("You have selected:")
 st.write("Category:", category)
 st.write("Sub-category:", sub_category)
 
@@ -50,19 +53,16 @@ st.write("Sub-category:", sub_category)
 
 
 # User inputs for others remaining
-region = st.selectbox("Region", ["Central", "East", "South", "West"])
+region = st.selectbox("Region", region_lst)
 
-segment = st.selectbox("Customer Segment", ["Consumer", "Corporate", "Home Office"])
+segment = st.selectbox("Customer Segment", segment_lst)
 
-ship_mode = st.selectbox(
-    "Ship Mode",
-    ["First Class", "Same Day", "Second Class", "Standard Class"]
-)
+ship_mode = st.selectbox("Ship Mode", shipmode_lst)
 
-# Date-based inputs
-order_year = st.number_input("Order Year", min_value=2014, max_value=2030, value=2018)
-order_month = st.slider("Order Month", 1, 12, 6)
-order_day = st.slider("Order Day", 1, 31, 15)
+# # Date-based inputs   (COMMENTING IT AS USING THE OLD_MODEL ONLY WHICH DON'T HAVE THESE)
+# order_year = st.number_input("Order Year", min_value=2014, max_value=2030, value=2018)
+# order_month = st.slider("Order Month", 1, 12, 6)
+# order_day = st.slider("Order Day", 1, 31, 15)
 
 # Predict button
 if st.button("Predict Sales"):
@@ -73,9 +73,9 @@ if st.button("Predict Sales"):
         "Region": [region],
         "Segment": [segment],
         "Ship Mode": [ship_mode],
+        "Order Year": [order_year],
         "Order Month": [order_month],
-        "Order Day": [order_day],
-        "Order Year": [order_year]
+        "Order Day": [order_day]
     })
 
     prediction = model.predict(input_data)
--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from data_store import category_dict
 
 
 
-# Load trained model
-model = joblib.load("model.pkl")
+# Load trained model (using old model pickle file)
+model = joblib.load("data/model_RandomForestRegressor.pkl")
 
 
 
@@ -59,10 +59,10 @@ segment = st.selectbox("Customer Segment", segment_lst)
 
 ship_mode = st.selectbox("Ship Mode", shipmode_lst)
 
-# Date-based inputs
-order_year = st.number_input("Order Year", min_value=2014, max_value=2030, value=2018)
-order_month = st.slider("Order Month", 1, 12, 6)
-order_day = st.slider("Order Day", 1, 31, 15)
+# # Date-based inputs   (COMMENTING IT AS USING THE OLD_MODEL ONLY WHICH DON'T HAVE THESE)
+# order_year = st.number_input("Order Year", min_value=2014, max_value=2030, value=2018)
+# order_month = st.slider("Order Month", 1, 12, 6)
+# order_day = st.slider("Order Day", 1, 31, 15)
 
 # Predict button
 if st.button("Predict Sales"):
@@ -72,10 +72,10 @@ if st.button("Predict Sales"):
         "Sub-Category": [sub_category],
         "Region": [region],
         "Segment": [segment],
-        "Ship Mode": [ship_mode],
-        "Order Year": [order_year],
-        "Order Month": [order_month],
-        "Order Day": [order_day]
+        "Ship Mode": [ship_mode]
+        # "Order Year": [order_year],
+        # "Order Month": [order_month],
+        # "Order Day": [order_day]
     })
 
     prediction = model.predict(input_data)
